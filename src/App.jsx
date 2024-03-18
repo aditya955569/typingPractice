16c02acf8fd12e34abc65f444c09798cbf394e12
aditya955569 <aditya.kandpal2016@gmail.com>
2024-03-18 18:06:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react"
+import axios from "axios"
+import { useEffect } from "react"
+import Popup from "reactjs-popup";
+function App() {
+  const [index, setIndex] = useState(0);
+  const [time, setTime] = useState(10)
+  const [sometext, setSometext] = useState("")
+  const [userInput, setUserInput] = useState("")
+  const [score, setScore] = useState(0);
+  const [timetwo, setTimetwo] = useState(time);
+  const [g, setG] = useState(false);
+  useEffect(() => {
+    let myinterval = setInterval(() => {
+      if (time > 0) {
+        setTime(time => time - 1);
+      }
+      else {
+        clearInterval(myinterval);
+        setTimeout(() => {
+          fetchRandomWord();
+          setIndex(0);
+          setScore(0);
+          setSometext("");
+          setUserInput("");
+          setTime(10);
+          setTimetwo(10);
+        }, 5000);
+      }
+    }, 1000)
+    return () => {
+      clearInterval(myinterval);
+    }
+  }
+  )
+  useEffect(() => {
+    fetchRandomWord();
+  }, []);
+  function processInput(value) {
+    console.log(userInput);
+    if (value.endsWith(sometext[index])) {
+      console.log(true);
+      setIndex(index => index + 1);
+      setUserInput(value);
+      if (index == sometext.length - 1) {
+        setUserInput('');
+        setIndex(0);
+        fetchRandomWord();
+        setScore(score => score + 1);
+        setTime(time => time - 2);
+      }
+    }
+    else {
+      setTime(timetwo);
+      setUserInput('');
+      setIndex(0);
+    }
+  }
+  const fetchRandomWord = () => {
+    axios({
+      method: 'get',
+      url: 'https://random-word-api.herokuapp.com/word',
+    })
+      .then(function (response) {
+        const resp = response.data
+        const data = resp[0]
+        setSometext(data);
+        setTimetwo(time);
+      })
+  }
+  return (
+    <div className="h-screen bg-slate-400">
+      <div className="grid grid-cols-2 gap-2">
+        <div className="text-5xl font-bold font-serif pl-5 pt-5 text-red-700">
+          TypingPractice
+        </div>
+        <div className="flex justify-end text-4xl font-bold pr-5 pt-5">
+          Time Left: {time} sec
+        </div>
+      </div>
+      <div className="flex justify-end text-4xl font-bold pr-5 pt-5">Score: {score}</div>
+      <div className="flex w-full justify-center">
+        <div className="text-3xl font-semibold pt-2">{sometext}</div>
+      </div>
+      <div className="flex w-full justify-center text-3xl pt-3">
+        <input type="text" value={userInput} onChange={
+          (e) => processInput(e.target.value)
+        } ></input>
+      </div>
+    </div>
+  )
+}
+
+export default App
